8ef7ad8e1dcda593231f0fa3e9f3770b01094fcb
SillyCoon <self-councious@proton.me>
2025-12-16 11:44:58 +0900

    add example with error without handling

--- app/_components/dnd-container.tsx
+++ app/_components/dnd-container.tsx
@@ -23,4 +23,29 @@ export const DndContainer = () => {
 
   return <DndExample items={items} onOrderChange={handleOrderChange} />;
 };
+
+export const DndContainerWithError = () => {
+  const [items] = useState([
+    { id: "item-1", text: "Item 1" },
+    { id: "item-2", text: "Item 2" },
+    { id: "item-3", text: "Item 3" },
+  ]);
+
+  const handleOrderChange = async (
+    _newOrder: { id: string; text: string }[],
+  ) => {
+    // Simulate a server request with a delay
+    return new Promise<void>((_resolve, reject) => {
+      setTimeout(() => {
+        reject();
+      }, 1000);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Drag and Drop Example with error</h1>
+      <DndExample items={items} onOrderChange={handleOrderChange} />
+    </div>
+  );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import styles from "./page.module.css";
-import { DndContainer } from "./_components/dnd-container";
+import {
+  DndContainer,
+  DndContainerWithError,
+} from "./_components/dnd-container";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <DndContainer />
+        <DndContainerWithError />
       </main>
     </div>
   );
