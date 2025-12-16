430ce224924a66d5bef4888e2bbf9594247b8b4b
SillyCoon <self-councious@proton.me>
2025-12-16 11:24:00 +0900

    add basic dnd-example based on dnd-kit sortable

---
+++ app/_components/dnd-example.tsx
@@ -0,0 +1,80 @@
+"use client";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import {
+  DndContext,
+  type DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import styles from "./index.module.css";
+import { useId, useState } from "react";
+
+export function SortableItem(props: { id: string; text: string }) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className={styles.item}
+      {...attributes}
+      {...listeners}
+    >
+      {props.text}
+    </div>
+  );
+}
+
+export const DndExample = () => {
+  const [items, setItems] = useState([
+    { id: "item-1", text: "Item 1" },
+    { id: "item-2", text: "Item 2" },
+    { id: "item-3", text: "Item 3" },
+  ]);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over?.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+
+  return (
+    <DndContext id={useId()} onDragEnd={handleDragEnd} sensors={sensors}>
+      <SortableContext items={items}>
+        <div>
+          {items.map(({ id, text }) => (
+            <SortableItem key={id} id={id} text={text} />
+          ))}
+        </div>
+      </SortableContext>
+    </DndContext>
+  );
+};
